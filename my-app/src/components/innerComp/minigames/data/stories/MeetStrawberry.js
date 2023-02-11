962739d4792751f9defe49e0ca0acd5ee676774a
Delaney Edwards <delaney.l.edwards@gmail.com>
2023-02-11 14:38:53 -0800

    Updated JSON from scene creation

--- my-app/src/components/innerComp/minigames/data/stories/MeetStrawberry.js
+++ my-app/src/components/innerComp/minigames/data/stories/MeetStrawberry.js
@@ -1,179 +1,139 @@
 const MEET_STRAWBERRY = {
-  "MeetStrawberry": {
+  "testScene": {
     "type": "comic",
-    "background": "beach-big",
-    "nextScene": "meetstrawberry-towater",
-    "baseFrame": [
-      {
-        "type": "sprite",
-        "image": "strawberry",
-        "x": 25.799999999999997,
-        "y": 33.7,
-        "size": "40",
-        "flipX": false
-      }
-    ],
-    "frames": [
-      [],
-      []
-    ],
-    "dialogue": [
-      {
-        "speaker": "Strawberry",
-        "message": "Hi there! My name is Strawberry. I’m a Harbor Seal and I live in the Puget Sound.",
-        "type": "left"
-      },
-      {
-        "speaker": "Strawberry",
-        "message": "Welcome to my favorite beach! It’s the Penn Cove Beach on Whidbey Island.",
-        "type": "left"
-      }
-    ]
-  },
-  "MeetStrawberry-towater": {
-    "type": "comic",
-    "background": "beach-small",
-    "nextScene": "meetstrawberry-inwater",
+    "background": "sea",
+    "nextScene": "testScene2",
     "baseFrame": [],
     "frames": [
       [
         {
           "type": "sprite",
-          "image": "strawberry",
-          "x": "25.7",
-          "y": "037",
-          "size": "040",
+          "image": "pancake-flapjack-octopus",
+          "x": 0,
+          "y": 0,
+          "size": 50,
           "flipX": false
         },
         {
           "type": "sprite",
-          "image": "lighter",
-          "x": 92.5,
-          "y": 39.3,
-          "size": "03",
-          "flipX": false
-        },
-        {
-          "type": "sprite",
-          "image": "fishingline",
-          "x": 84.8,
-          "y": 43.1,
-          "size": "015",
-          "flipX": false
-        },
-        {
-          "type": "sprite",
-          "image": "blue-bag",
-          "x": 93.89999999999999,
-          "y": 38.5,
-          "size": "010",
+          "image": "user-placeholder",
+          "x": 55.4,
+          "y": -6.799999999999999,
+          "size": 50,
           "flipX": false
         }
       ]
-    ],
-    "dialogue": [
-      {
-        "speaker": "Strawberry",
-        "message": "I come here all the time to bask in the sun and meet up with my animal friends!",
-        "type": "left"
-      }
     ]
   },
-  "MeetStrawberry-inwater": {
+  "pancakeIntro": {
     "type": "comic",
-    "background": "sea-mountains",
-    "nextScene": "GOTO /play?next=squawky",
-    "baseFrame": [
-      {
-        "type": "sprite",
-        "image": "blue-bag",
-        "x": 72,
-        "y": 40.1,
-        "size": "20",
-        "flipX": false
-      },
-      {
-        "type": "sprite",
-        "image": "lighter",
-        "x": 68.1,
-        "y": 37.3,
-        "size": "06",
-        "flipX": false
-      },
-      {
-        "type": "sprite",
-        "image": "fishingline",
-        "x": 61.79999999999999,
-        "y": 46.9,
-        "size": "30",
-        "flipX": false
-      }
-    ],
+    "background": "sea",
+    "nextScene": "testScene2",
+    "baseFrame": [],
     "frames": [
       [
         {
           "type": "sprite",
-          "image": "strawberry-surprised",
-          "x": 12.099999999999998,
-          "y": "32.4",
-          "size": "43",
+          "image": "user-placeholder",
+          "x": 49.900000000000006,
+          "y": -0.8999999999999986,
+          "size": 50,
+          "flipX": false
+        },
+        {
+          "type": "sprite",
+          "image": "pancake-flapjack-octopus",
+          "x": 8.600000000000003,
+          "y": "35",
+          "size": "15",
           "flipX": false
         }
       ],
       [
         {
           "type": "sprite",
-          "image": "strawberry",
-          "x": 12.2,
-          "y": 35.00000000000001,
-          "size": "040",
+          "image": "user-placeholder",
+          "x": 49.900000000000006,
+          "y": -0.8999999999999986,
+          "size": 50,
           "flipX": false
-        }
-      ],
-      [
+        },
         {
           "type": "sprite",
-          "image": "strawberry-sad",
-          "x": "11.800000000000004",
-          "y": "034.2",
-          "size": "40",
-          "flipX": false
-        }
-      ],
-      [
-        {
-          "type": "sprite",
-          "image": "strawberry",
-          "x": "11.800000000000004",
-          "y": "034.2",
-          "size": "40.3",
+          "image": "pancake-flapjack-octopus",
+          "x": 8.600000000000003,
+          "y": "35",
+          "size": "15",
           "flipX": false
         }
       ]
     ],
     "dialogue": [
       {
-        "speaker": "Strawberry",
-        "message": "Oh no! Is that more marine debris?",
+        "speaker": "",
+        "message": "Hello there! I’m Pancake the Flapjack Octopus. \nPress the arrow in the bottom right to keep going!",
         "type": "left"
       },
       {
-        "speaker": "Strawberry",
-        "message": "Recently, a lot of humans have been leaving their trash on the beach. When it gets swept into the water, scientists call it marine debris.",
-        "type": "left"
-      },
-      {
-        "speaker": "Strawberry",
-        "message": "Marine debris can harm me and my friends in different ways depending on the characteristics. Seeing all this debris makes me worried for my friends.",
-        "type": "left"
-      },
-      {
-        "speaker": "Strawberry",
-        "message": "Let’s go check on them together!",
+        "speaker": "",
+        "message": " I’ll lead you around your adventure today and help you learn about the ocean, sea creatures and the impact humans have on the ocean.",
         "type": "left"
       }
     ]
   },
-};
+  "sallyTalking": {
+    "type": "comic",
+    "background": "sea",
+    "nextScene": "testScene2",
+    "baseFrame": [
+      {
+        "type": "sprite",
+        "image": "sally-salmon",
+        "x": "14",
+        "y": "30",
+        "size": "15",
+        "flipX": true
+      },
+      {
+        "type": "sprite",
+        "image": "user-placeholder",
+        "x": 55.099999999999994,
+        "y": -3.1999999999999993,
+        "size": 50,
+        "flipX": false
+      }
+    ],
+    "frames": [
+      [],
+      [],
+      [],
+      []
+    ]
+  },
+  "sallyTalkingA": {
+    "type": "comic",
+    "background": "sea",
+    "nextScene": "testScene2",
+    "baseFrame": [
+      {
+        "type": "sprite",
+        "image": "user-placeholder",
+        "x": 54.6,
+        "y": -1,
+        "size": 50,
+        "flipX": false
+      },
+      {
+        "type": "sprite",
+        "image": "sally-salmon",
+        "x": 10.000000000000002,
+        "y": 10.500000000000002,
+        "size": "20",
+        "flipX": true
+      }
+    ],
+    "frames": []
+  }
+}
 
 export default MEET_STRAWBERRY;
