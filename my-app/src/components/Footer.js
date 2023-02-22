13eddd480eaf484b3b31292637a9c7684511c89c
Brian Ho <brianho34@yahoo.com>
2023-02-22 13:05:43 -0800

    updated about page

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import wendywhale from "../imgs/github.png";
+import profileborder from "../imgs/ischool-logo.png";
+const Footer = () => {
+  return (
+    <div className="foot-section border-top">
+      <span>Marine Rescue: The Sea-quel</span>
+      <div className="foot-title">
+        <span>Copyright FlapJack</span>
+        <img className="'
+        " src={require( "../imgs/github.png")} />
+      </div>
+      <img className="" src={require("../imgs/ischool-logo.png")} />
+    </div>
+  );
+};
+export default Footer;
+
+
+
+
