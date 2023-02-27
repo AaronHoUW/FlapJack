56651f19f01840ebfa9959ee7f3f4d7a1ddbb72c
Brian Ho <brianho34@yahoo.com>
2023-02-27 14:03:47 -0800

    redirect guidepage

--- my-app/src/components/FlapGuide.js
+++ my-app/src/components/FlapGuide.js
@@ -6,8 +6,10 @@ import bubble from "../imgs/bubbles.png";
 import pancake from "../imgs/flapjack-guide.png";
 import userPlaceholder from "../imgs/user.png";
 import fish from "../imgs/sal.png";
+import {useNavigate } from 'react-router-dom'
 
 const App = () => {
+  const navigate = useNavigate();
   return (<>
     <img src={sea} style={{width: '100%' }} />;
     <div>
@@ -15,7 +17,7 @@ const App = () => {
       <img className="talk" src={require("../imgs/interaction.png")} />
       <img className="excel" src={require("../imgs/excel.png")} />
       <img className="bubble" src={require("../imgs/bubbles.png")} />
-      <img className="fish" src={require("../imgs/sal.png")} />
+      <img className="fish" src={require("../imgs/sal.png") } onClick={() => navigate('/level1')} />
       <img className="flapjack-guide" src={require("../imgs/flapjack-guide.png")} />
       <img className="user-placeholder" src={require("../imgs/user.png")} />
     </div>
