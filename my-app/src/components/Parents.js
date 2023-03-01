287253e543d24e8c532d978d46d888af379ae356
Brian Ho <brianho34@yahoo.com>
2023-03-01 11:05:27 -0800

    updated parent page

--- my-app/src/components/Parents.js
+++ my-app/src/components/Parents.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {
+    FooterContainer,
+    FooterText,
+    FooterCopyright,
+} from './Home/styles.tsx';
+import "../index.css";
+
 
 function Parents() {
     return (
+        <>
         <div className='parent-what-is-section'>
-            <div className='container'>
+            <div className=''>
                 {/* What is Marine Rescue? */}
                 <div className='row what-content-height'>
                     <div className='col my-auto'>
@@ -32,9 +40,50 @@ function Parents() {
                         </p>
                     </div>
                 </div>
+                <div className="about-parent">
+                        <span className="flap-team mb-1">What will my child learn using Marine Rescue?</span>
+                        <span className="four-members col-xs-12 col-md-10 mt-2 ms-2">
+                        Marine Rescue is more than entertainment for you child, it will teach them about the ocean, the animals that live there, and how they are affected by pollution.
+                        </span>
+                        <div className="profile-group row">
+                            <div className="profile col text-center mt-4">
+                                <img className="picture-one" src='./imgs/profile-border.png' />
+                                <div className="profile-body">
+                                    <h5 className="profile-name col-xs-12 col-md-12 mt-5">Marine Life</h5>
+                                    <p className="profile-role col-xs-12 col-md-12 mt-4">Your child will learn about local PNW marine life such as salmon, whales, and crabs. They will learn fun facts about the animals, and about their experiences.</p>
+                                </div>
+                            </div>
+                            <div className="profile col text-center mt-4">
+                                <img className="picture-one" src='./imgs/profile-border.png' />
+                                <div className="profile-body text-center">
+                                    <h5 className="profile-name col-xs-12 col-md-12 mt-5">Marine Pollution</h5>
+                                    <p className="profile-role col-xs-12 col-md-12 mt-4">Your child will learn about how marine pollution such as ghost nets and trash impacts marine life. They will also learn how they can help to clean up marine debris.</p>
+                                </div>
+                            </div>
+                            <div className="profile col text-center mt-4">
+                                <img className="picture-one" src='./imgs/profile-border.png' />
+                                <div className="profile-body">
+                                    <h5 className="profile-name col-xs-12 col-md-12 mt-5">Vocabulary</h5>
+                                    <p className="profile-role col-xs-12 col-md-12 mt-4 ">Your child will learn vocabulary words related to marine life and marine debris to help support their learning. They will also learn how to pronounce the words.</p>
+                                </div>
+                                </div>
+                        </div>
+                    </div>
             </div>
         </div>
+                <FooterContainer>
+                <FooterText>Marine Rescue: The Sea-quel</FooterText>
+                <FooterCopyright>
+                    <p>Copyright &copy; FlapJack</p>
+                    <img src='./imgs/mdi_github.png' alt='Github logo' />
+                </FooterCopyright>
+                <img src='./imgs/iSchool-logo.png' alt='logo of the Information School at the University of Washington' />
+            </FooterContainer>
+        </>
+        
     );
+    
 }
+
 
 export default Parents;
