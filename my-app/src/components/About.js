b801283b840ed121309da6d06a25d38a05557d8f
Brian Ho <brianho34@yahoo.com>
2023-03-01 20:10:22 -0800

    edit about page

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -11,15 +11,15 @@ const About = () => {
         <>
             <main>
                 <div className="about-us">
-                    <div className="info-section ">
-                        <span className="the-goal">The Goal</span>
-                        <span className="marine-rescue col-xs-5 m-3">
+                    <div className="info-section">
+                        <span className="the-goal m-1 mb-4">The Goal</span>
+                        <span className="marine-rescue m-1 mb-4">
                             Marine Rescue is a platform for elementary students to learn about
                             debris&#39; impacts on marine wildlife. This is the second iteration
                             of Marine Rescue, built as the final product of our Informatics
                             capstone. 
                             </span>
-                            <span className="marine-rescue col-xs-5 m-4">
+                            <span className="marine-rescue">
                             Through interactive educational stories and activities,
                             students can develop a sense of connection to the nature of the
                             Pacific North West through active learning and educational games.
@@ -34,40 +34,40 @@ const About = () => {
                     <img className="wendywhale" src='./sprites/sprite-wendy-whale.png' />
                 </div>
                 <div className="about-team cotainer">
-                        <span className="flap-team mb-1">Our Team</span>
-                        <span className="four-members col-xs-12 col-md-10 mt-2 ms-2">
+                        <span className="flap-team">Our Team</span>
+                        <span className="four-members m-1 mb-5">
                             We are a team of four students studying Informatics at the University of
                             Washington’s Information School.
                         </span>
                         <div className="profile-group row">
-                            <div className="profile col text-center mt-4">
+                            <div className="profile col text-center">
                                 <img className="picture-one" src='./imgs/profile-border.png' />
                                 <div className="profile-body">
-                                    <h5 className="profile-name col-xs-12 col-md-12 mt-5">Aaron Ho</h5>
-                                    <p className="profile-role col-xs-12 col-md-12 mt-4">Software Engineer</p>
+                                    <h5 className="profile-name">Aaron Ho</h5>
+                                    <p className="profile-role ">Software Engineer</p>
                                 </div>
                             </div>
-                            <div className="profile col text-center mt-4">
+                            <div className="profile col text-center">
                                 <img className="picture-one" src='./imgs/profile-border.png' />
                                 <div className="profile-body text-center">
-                                    <h5 className="profile-name col-xs-12 col-md-12 mt-5">Brian Ho</h5>
-                                    <p className="profile-role col-xs-12 col-md-12 mt-4">User Researcher</p>
+                                    <h5 className="profile-name">Brian Ho</h5>
+                                    <p className="profile-role">User Researcher</p>
                                         <p className="profile-center"> &amp; </p>
-                                        <p className="profile-role">Software Engineer</p>
+                                        <p className="profile-role  m-1 mb-5">Software Engineer</p>
                                 </div>
                             </div>
-                            <div className="profile col text-center mt-4">
+                            <div className="profile col text-center">
                                 <img className="picture-one" src='./imgs/profile-border.png' />
                                 <div className="profile-body">
-                                    <h5 className="profile-name col-xs-12 col-md-12 mt-5">Delaney Edwards</h5>
-                                    <p className="profile-role col-xs-12 col-md-12 mt-4 ">UX Designer</p>
+                                    <h5 className="profile-name">Delaney Edwards</h5>
+                                    <p className="profile-role">UX Designer</p>
                                 </div>
                                 </div>
-                                <div className="profile col text-center mt-4">
+                                <div className="profile col text-center">
                                     <img className="picture-one" src='./imgs/profile-border.png' />
                                     <div className="profile-body">
-                                        <h5 className="profile-name col-xs-12 col-md-12 mt-5">Jun Nguyen</h5>
-                                        <p className="profile-role col-xs-12 col-md-12 mt-4">Project Manager</p>
+                                        <h5 className="profile-name">Jun Nguyen</h5>
+                                        <p className="profile-role">Project Manager</p>
                                         <p className="profile-center"> &amp; </p>
                                         <p className="profile-role">Software Engineer</p>
                                 </div>
@@ -76,16 +76,16 @@ const About = () => {
                     </div>
                 <div className="ack-section">
                     <span className="last-section mb-3">Acknowledgements</span>
-                    <span className="shout-out  m-1 mb-3">
+                    <span className="shout-out  m-1 mb-4">
                         Our team is grateful for the support from our mentors and peers at the
                         University of Washington’s Information School for their advice through
                         each iteration of our project.
                         </span> 
-                        <span className=" shout-out  m-1 mb-3">
+                        <span className=" shout-out  m-1 mb-4">
                         Throughout this project, we were supported by Andrey Butenko, the software engineer from the team that
                         worked on the first iteration of Marine Rescue. 
                         </span> 
-                        <span className=" shout-out m-1 mb-3">
+                        <span className=" shout-out m-1 mb-4">
                         We were also greatly supported by two professors, Nam-Ho Park and Marlina Hales. They both
                         provided valuable feedback throughout our capstone project.
                         </span>
