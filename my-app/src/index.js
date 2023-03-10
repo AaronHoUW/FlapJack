404bf2e1b4186071d55fa4bb0c13b5f13e7389aa
aaronho@uw.edu <aaronho@aaronh.local>
2023-03-09 16:40:46 -0800

    Added firebase hosting

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5WfXtbNmL7WhY07KLc5ZaXKsx6c_wh24",
+  authDomain: "marine-rescue-seaquel.firebaseapp.com",
+  projectId: "marine-rescue-seaquel",
+  storageBucket: "marine-rescue-seaquel.appspot.com",
+  messagingSenderId: "52813694511",
+  appId: "1:52813694511:web:782d074ac79b2e387ba8da",
+  measurementId: "G-92M0XHFNDL"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
