a1513fb7e8c027b3dcd054609a6717445c5d3234
Jun Nguyen <soshiyo@uw.edu>
2023-04-06 16:47:05 -0700

    Updated nets to bob up and down

--- my-app/src/components/innerComp/minigames/data/games/NetMiniGame.tsx
+++ my-app/src/components/innerComp/minigames/data/games/NetMiniGame.tsx
@@ -20,6 +20,28 @@ export const User = styled.img<Props>`
 
 export const Net = styled.img<Props>`
     position: relative;
+    animation: MoveUpDown 1s linear infinite;
+    
+    @keyframes MoveUpDown {
+        0%, 100% {
+            transform: translateY(0);
+        }
+        50% {
+            transform: translateY(-25px);
+        }
+    }
+
+    :nth-child(3) {
+        animation-delay: 300ms;
+    }
+
+    :nth-child(4) {
+        animation-delay: 500ms;
+    }
+
+    :nth-child(5) {
+        animation-delay: 100ms;
+    }
 `;
 
 // export const Modal = styled.div`
