72153455513e991cfd9f7a4b4d00ad205c68c07d
btho1 <btho1@uw.edu>
2023-04-17 17:50:43 -0700

    add images

---
+++ my-app/src/components/About/About.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import {
+    FooterContainer,
+    FooterText,
+    FooterCopyright,
+} from './styles.tsx';
+
+
+function Parents() {
+    return (
+        <>
+ <div className='level-one'>
+                 <div className='seagull'>
+                     <div className='row what-content-height'>
+                        <div className='col my-auto'>
+                            <h2 className='first-title'>Level 1: Meet Shawn the Seagull</h2>
+                            <p className='first-info'> Meet Shawn and learn about what Trash Islands are. Maybe you can even help clean up trash on the beach!
+                            </p>
+                            <a href="/level1">
+                                <button className="link-button">Play</button>
+                                </a>
+                        </div>
+                       <div className='col'>
+                           <img className='shaw-level' src='./imgs/shawn.png' alt="Shawn The Seagull"></img>
+                      </div>
+                   </div>
+                   </div>
+                     <div className='level-two'>
+               <div className='salmon'>
+                   <div className='row what-content-height'>
+                      <div className='col my-auto'>
+                        <h2 className='second-title'>Level 2: Meet Sally the Salmon</h2>
+                         <p className='second-info'> Meet Sally and learn about Salmon’s experience of the Elwha. Maybe you can even help clean up ghosts nets and help out fish and other animals in the sea!
+                         </p>
+                         <a href="/level2">
+                                <button className="link-button">Play</button>
+                                </a>
+                       </div>
+                       <div className='col'>
+                       <img className='sal-level' src='./imgs/sal.png' alt="Sally the Salmon"></img>
+                       </div>
+                  </div>
+                   </div>
+                </div>
+<div className='level-three'>
+               <div className='whale'>
+                   <div className='row what-content-height'>
+                        <div className='col my-auto'>
+                         <h2 className='third-title'>Level 3: Meet Wendy the Whale</h2>
+                            <p className='third-info'> Meet Shawn and learn about the whale and the plastic that they eat. Maybe you can even help get rid of plastic inside the whale’s body!
+                           </p>
+                           <a href="/level3">
+                                <button className="link-button">Play</button>
+                                </a>
+                       </div>
+                       <div className='col'>
+                         <img className='wen-level' src='./imgs/final-wendy.png' alt="Wendy the Whale"></img>
+                     </div>
+                    </div>
+             </div>
+</div>
+
+</div>
+            <FooterContainer>
+            <FooterText>Marine Rescue: The Sea-quel</FooterText>
+            <FooterCopyright>
+                <p>Copyright &copy; FlapJack</p>
+                <img src='./imgs/mdi_github.png' alt='Github logo' />
+            </FooterCopyright>
+            <img src='./imgs/iSchool-logo.png' alt='logo of the Information School at the University of Washington' />
+        </FooterContainer>
+        </>
+);
+
+}
+
+export default Parents;
---
+++ my-app/src/components/About/styles.tsx
@@ -0,0 +1,209 @@
+import styled from 'styled-components';
+
+export const IntroContainer = styled.div`
+    background-color: #1E7FA4;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 80px 40px;
+    width: 100%;
+    height: 60vh;
+    font-family: 'Mulish';
+    padding-bottom: 0;
+`;
+
+export const IntroText = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 40%;
+    margin-left: 100px;
+`;
+
+export const IntroTitle = styled.h2`
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    color: white;
+    width: 100%;
+    height: 60px;
+`;
+
+export const IntroDescription = styled.p`
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 25px;
+    color: white;
+`;
+
+export const IntroSprites = styled.div`
+    height: 300px;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+    .pancake {
+        width: 200px;
+    }
+
+    .user {
+        width: 500px;
+    }
+`;
+
+export const HelpContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 80px 40px;
+    padding-top: 0;
+
+    width: 100%;
+    height: 80vh;
+
+    background-color: #1E7FA4;
+    font-family: 'Mulish';
+`;
+
+export const HelpText = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+    width: 40%;
+    margin-right: 100px;
+`;
+
+export const HelpDescription = styled.p`
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 25px;
+    color: white;
+    margin-top: 30px;
+`;
+
+export const HelpVideoContainer = styled.div`
+    width: 60%;
+    height: 100%;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    margin-right: 100px;
+    margin-left: 100px;
+    display: flex;
+    align-items: center;
+`;
+
+export const HelpVideo = styled.iframe`
+`;
+
+export const LearnContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 40px;
+    gap: 46px;
+
+    width: 100%;
+    height: 80vh;
+
+    background-color: #85C9E3;
+    font-family: 'Mulish';
+`;
+
+export const LearnText = styled.div`
+    width: 100%;
+`;
+
+export const LearnTitle = styled.h2`
+    font-weight: 400;
+    font-size: 48px;
+    line-height: 60px;
+    color: black;
+    width: 100%;
+    height: 60px;
+`;
+
+export const LearnDescription = styled.p`
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 25px;
+    color: black;
+    margin-top: 30px;
+`;
+
+export const CardsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0;
+
+    width: 100%;
+`;
+
+export const CardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0;
+    text-align: center;
+
+    width: 33%;
+
+    img {
+        width: 150px;
+    }
+
+    h4 {
+        font-weight: 400;
+        font-size: 36px;
+        line-height: 45px;
+        color: black;
+        margin: 20px;
+    }
+`;
+
+export const FooterContainer = styled.div`
+    display: flex;
+    border-top: 1px solid white;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 40px;
+    gap: 15px;
+
+    width: 100%;
+    height: 15vh;
+    background: #1E7FA4;
+    font-family: 'Mulish';
+`;
+
+export const FooterText = styled.p`
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 25px;
+    color: white;
+    margin: 0;
+`;
+
+export const FooterCopyright = styled.div`
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 25px;
+    color: white;
+    margin: 0;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    p {
+        margin: 0;
+        margin-right: 10px;
+    }
+`;
